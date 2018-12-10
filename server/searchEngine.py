1657d972e657f58bf60c2f7a3a40249c9d26d98c
Jesper <[jp222vq@student.lnu.se]>
2018-12-11 00:36:58 +0100

    now searchengine returns correct results

--- server/searchEngine.py
+++ server/searchEngine.py
@@ -3,11 +3,12 @@ import operator
 from db import db
 
 class score:
-    def __init__(self, page=None, score=0.0, content={}, location={}):
+    def __init__(self, page=None, score=0.0, content={}, location={}, pageRank={}):
         self.page = page
         self.score = score
         self.content = content
         self.location = location
+        self.pageRank = pageRank
 
 class searchEngine:
     def __init__(self, db=db()):
@@ -15,36 +16,27 @@ class searchEngine:
 
     def searchQuery(self, query):
         result = {}
-        totscore = score(content={}, location={})
+        totscore = score(content={}, location={}, pageRank={})
         pages = self.db.getPages()
+        id = self.getIdForWord(query)
 
-        for link in pages:
-            p = pages[link]
-            id = self.getIdForWord(query)
-
-            if id in p.wordlocations:
-                totscore.content[p.link] = self.db.getFrequencyScore(p, id)
-                totscore.location[p.link] = self.db.getLocationScore(p, id) + 1
-            else: 
-                totscore.content[p.link] = 0
-                totscore.location[p.link] = 100000
+        [self.setScores(totscore, pages[p], id) for p in pages]
 
         self.normalizeScore(totscore.content, False)
         self.normalizeScore(totscore.location, True)
+        self.normalizeScore(totscore.pageRank, False)
 
-        for link in pages:
-            p = pages[link]
-            calcScore = 1.0 * totscore.content[p.link] + 0.5 * totscore.location[p.link]
+        for p in pages.values():
+            calcScore = totscore.content[p.link] + (0.8 * totscore.location[p.link]) + (0.5 * totscore.pageRank[p.link])
             result[p.link] = score(page=p, score=calcScore)
 
         # Sort list from top down highest score
         sortedList = []
         for page in (sorted(result.values(), key=operator.attrgetter('score'), reverse=True)):
             sortedList.append(page)
-            
+
         # Return top 5
         return sortedList[:5]
-
 
     def getIdForWord(self, word):
         wordid = self.db.getWordId(word)
@@ -56,16 +48,27 @@ class searchEngine:
             self.db.saveWord(wordid, word)
             return wordid
 
+
+    def setScores(self, totscore, p, id):
+        if id in p.wordlocations:
+            totscore.content[p.link] = self.db.getFrequencyScore(p, id)
+            totscore.location[p.link] = self.db.getLocationScore(p, id) + 1
+        else: 
+            totscore.content[p.link] = 0
+            totscore.location[p.link] = 100000
+        totscore.pageRank[p.link] = p.pageRank
+
     def normalizeScore(self, scores, smallIsBetter):
         if smallIsBetter:
+            vsmall = 0.00001
             vmin = min(scores.values())
             for key in scores:
                 score = scores[key]
-                scores[key] = float(vmin) / max(score, 0.00001)
+                scores[key] = float(vmin) / max(vsmall, score)
         else:
             vmax = max(scores.values())
             for key in scores:
                 score = scores[key]
                 scores[key] = score / vmax
 
-# print(searchEngine().searchQuery('nintendo'))
+print(searchEngine().searchQuery('nintendo'))
